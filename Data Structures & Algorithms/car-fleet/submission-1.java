ca2cac7dcb3d597c453c696f4b805f05fb555ad1
Dhruv Gambhir <75164918+dhruv-gambhir@users.noreply.github.com>
2026-08-02 23:39:27 +0530

    Add: car-fleet - submission-1

---
+++ Data Structures & Algorithms/car-fleet/submission-1.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int carFleet(int target, int[] position, int[] speed) {
+        int[][] pair = new int[position.length][2];
+        int count = 1;
+        for(int i=0; i<position.length; i++) {
+            pair[i][0] = position[i];
+            pair[i][1] = speed[i];
+        }
+        Arrays.sort(pair, (a, b) -> Integer.compare(b[0], a[0]));
+        Stack<Double> stk = new Stack<>();
+        stk.push((double) (target - pair[0][0]) / pair[0][1]);
+        
+        for(int i=1;i<position.length;i++) {
+            double curr = (double) (target - pair[i][0]) / pair[i][1];
+            if(curr>stk.peek()) {
+                stk.push(curr);
+            }
+        }
+        return stk.size();
+        
+        }
+
+    }
+
