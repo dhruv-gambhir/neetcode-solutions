f4b4611c9c52183e7fa8e95bac7fa9dd7f07511d
Dhruv Gambhir <75164918+dhruv-gambhir@users.noreply.github.com>
2026-08-04 18:26:17 +0530

    Add: eating-bananas - submission-3

---
+++ Data Structures & Algorithms/eating-bananas/submission-3.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        int l = 1;
+        int r = Arrays.stream(piles).max().getAsInt();
+        int res = r;
+
+        while(l<=r) {
+            int mid = l + (r-l)/2;
+            int count = 0;
+            for(int i:piles){
+                count += Math.ceil((double) i/mid);
+            }
+            if(count <= h) {
+                res = mid;
+                r = mid -1;
+            } else {
+                l = mid +1;
+            }
+        }
+        return res;
+    }
+}
